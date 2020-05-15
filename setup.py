e2db930b417fc6f058bd53bfb244d7b5398dc5ea
malteos <7977470+malteos@users.noreply.github.com>
2020-05-15 10:50:34 +0200

    Fix pip install

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from distutils.core import setup
 
+from setuptools import find_packages
+
 setup(
     name="pii-identifier",
     version="0.1.0a",
-    packages=["pii_identifier"],
+    packages=find_packages(exclude=["tests"]),
     include_package_data=True,
     license="MIT",
     description="Find personally identifiable information in German texts using NER and rule based matching.",
