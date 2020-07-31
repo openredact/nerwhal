2b4e654dd02a199f59a0e1a4e625272cb8b93d1c
langhabel <langhabel@users.noreply.github.com>
2020-07-31 18:14:43 +0200

    Add list_integrated_recognizers to public interface

--- nerwhal/__init__.py
+++ nerwhal/__init__.py
@@ -1,3 +1,4 @@
 from nerwhal.core import recognize, evaluate  # noqa: F401
 from nerwhal.types import NamedEntity, Config  # noqa: F401
 import nerwhal.recognizer_bases  # noqa: F401
+from nerwhal.integrated_recognizers import list_all as list_integrated_recognizers  # noqa: F401
