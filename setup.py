d6424b675b635c914500cab5349b2f7e4c4aa85a
langhabel <langhabel@users.noreply.github.com>
2020-07-31 16:58:03 +0200

    Specify semantic versioning ranges in install_requires

--- setup.py
+++ setup.py
@@ -10,5 +10,5 @@ setup(
     license="MIT",
     description="Find personally identifiable information in German texts using NER and rule based matching.",
     long_description=open("README.md").read(),
-    install_requires=["spacy==2.3.2", "stanza==1.0.1", "flashtext==2.7", "pydantic==1.6.1"],
+    install_requires=["spacy>=2.3.2,<3.0.0", "stanza>=1.0.1,<1.1.0", "flashtext>=2.7,<3.0", "pydantic>=1.0.0,<2.0.0"],
 )
