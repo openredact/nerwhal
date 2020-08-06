e63937e2d9e03ea6c1b34633f47d1e32ac598236
langhabel <langhabel@users.noreply.github.com>
2020-08-06 09:16:51 +0200

    Limit torch version in setup.py and requirements.txt
    Using 1.6.0 with stanza 1.0.1 leads to:
    RuntimeError: Integer division of tensors using div or / is no longer
    supported, and in a future release div will perform true division as in
    Python 3. Use true_divide or floor_divide (// in Python) instead.

--- setup.py
+++ setup.py
@@ -10,5 +10,11 @@ setup(
     license="MIT",
     description="Find personally identifiable information in German texts using NER and rule based matching.",
     long_description=open("README.md").read(),
-    install_requires=["spacy>=2.3.2,<3.0.0", "stanza>=1.0.1,<1.1.0", "flashtext>=2.7,<3.0", "pydantic>=1.0.0,<2.0.0"],
+    install_requires=[
+        "spacy>=2.3.2,<3.0.0",
+        "stanza>=1.0.1,<1.1.0",
+        "torch>=1.3.0,<1.6.0",
+        "flashtext>=2.7,<3.0",
+        "pydantic>=1.0.0,<2.0.0",
+    ],
 )
