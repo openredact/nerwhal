7afca9325c6ed64e3df141de12593c266762e1af
langhabel <langhabel@users.noreply.github.com>
2020-08-18 22:43:59 +0200

    Update version to 0.1.1a

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="nerwhal",
-    version="0.1.0a",
+    version="0.1.1a",
     url="https://openredact.org/",
     author="Jonas Langhabel",
     author_email="hello@openredact.org",
