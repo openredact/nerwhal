97eaef56d6de59718501095d631a0fb49700e45a
langhabel <langhabel@users.noreply.github.com>
2021-05-23 10:43:04 +0200

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="nerwhal",
-    version="0.1.1",
+    version="0.1.2",
     url="https://openredact.org/",
     author="Jonas Langhabel",
     author_email="hello@openredact.org",
